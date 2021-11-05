aed2d43520ce96fb8d36ddea108664da4f2ccb0b
koloczz <68149268+koloczz@users.noreply.github.com>
2021-11-05 10:24:23 +0100

    Added color toggle

--- SitAndEat/Scripts/main.js
+++ SitAndEat/Scripts/main.js
@@ -3,9 +3,11 @@ function showModal() {
     modal.style.display = "block";
 }
 
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
+function closeModal(element) {
+    if (element.classList.contains("modal") || element.classList.contains("close")) {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    }
 }
 
 function stickToTop() {
@@ -26,11 +28,19 @@ function stickToTop() {
 function updateTextInput(val) {
     document.getElementById('textInputHour').value=val; 
   }
+
 function updateTextInput2(val) {
     document.getElementById('textInputPerson').value=val; 
   }
+
 function makeSelection(target) {
     if (!target.classList.contains("taken-table")) {
         target.classList.toggle("selected-table");
     }
+}
+
+function changeTheme() {
+    var root = document.querySelector(":root");
+    var randomColor = Math.floor(Math.min(Math.random(), 0.9)*16777215).toString(16);
+    root.style.setProperty("--main-background-color", "#" + randomColor);
 }
