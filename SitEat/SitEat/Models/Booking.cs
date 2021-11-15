6ce1b3a82c6d179da22bddece19e74e323693832
pikuch <55052790+pikuch@users.noreply.github.com>
2021-11-15 13:13:42 +0100

    Add Booking model

---
+++ SitEat/SitEat/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitEat.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        [ForeignKey("Table")]
+        public int TableId { get; set; }
+        public DateTime Date { get; set; }
+        public int TimeStart { get; set; }
+        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+    }
+}
