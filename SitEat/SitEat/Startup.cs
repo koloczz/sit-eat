740419790d100f24c10d59808eed4f6623040e2c
koloczz <68149268+koloczz@users.noreply.github.com>
2021-11-15 13:33:13 +0100

    Added DB configuration

--- SitEat/SitEat/Startup.cs
+++ SitEat/SitEat/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SitEat.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace SitEat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SitEatContext>(
+                options => options.UseSqlServer("name=ConnectionStrings:SitEatContext"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
