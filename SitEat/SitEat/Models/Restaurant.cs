2b1bdf94e502154ae7a84c80bcb236274aa7188f
koloczz <68149268+koloczz@users.noreply.github.com>
2021-11-15 11:52:46 +0100

    Added Restaurant model

---
+++ SitEat/SitEat/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SitEat.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? ImagePath { get; set; }
+
+        [Required]
+        public string? Tags { get; set; }
+
+        [Required]
+        public string? OpeningTimes { get; set; }
+    }
+}
