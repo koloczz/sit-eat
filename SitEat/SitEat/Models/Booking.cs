2d2373459702423a2f7566a5ebd1624837028b27
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2021-11-16 10:06:12 +0100

    Add validation to Booking

--- SitEat/SitEat/Models/Booking.cs
+++ SitEat/SitEat/Models/Booking.cs
@@ -10,16 +10,20 @@ namespace SitEat.Models
     public class Booking
     {
         public int Id { get; set; }
-        [Required]
-        [ForeignKey("Table")]
+
+        [Required, ForeignKey("Table")]
         public int TableId { get; set; }
-        [Required]
+
+        [Required, DataType(DataType.Date)]
         public DateTime Date { get; set; }
-        [Required]
+
+        [Required, Display(Name = "Time Start")]
         public int TimeStart { get; set; }
-        [Required]
+
+        [Required, StringLength(30, MinimumLength = 1), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Name { get; set; }
-        [Required]
+
+        [Required, DataType(DataType.PhoneNumber)]
         public string Telephone { get; set; }
     }
 }
